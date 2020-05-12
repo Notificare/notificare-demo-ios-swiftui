f0885ef1376876732608296fce1c53d8f573f8d4
Helder Pinhal <helder@notifica.re>
2020-05-12 20:09:58 +0100

    Project setup

---
+++ Inbox/Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inbox
+//
+//  Created by Helder Pinhal on 12/05/2020.
+//  Copyright © 2020 Notificare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
