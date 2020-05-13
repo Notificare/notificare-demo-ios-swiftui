4509bb353ae940873ab0fd0f17fb832b7f01a92e
Helder Pinhal <helder@notifica.re>
2020-05-13 19:04:50 +0100

    Refactor the random colour for an item

--- Inbox/Source/Extensions/InboxItem+Color.swift
+++ Inbox/Source/Extensions/InboxItem+Color.swift
@@ -23,10 +23,23 @@ extension InboxItem {
 extension NotificareDeviceInbox {
     
     var color: Color {
-        let r = Int.random(in: 0...255)
-        let g = Int.random(in: 0...255)
-        let b = Int.random(in: 0...255)
+        var hash = 0
+        let colorConstant = 131
+        let maxSafeValue = Int.max / colorConstant
         
-        return Color(red: Double(r) / 255, green: Double(g) / 255, blue: Double(b) / 255)
+        for char in self.message.unicodeScalars {
+            if hash > maxSafeValue {
+                hash = hash / colorConstant
+            }
+            
+            hash = Int(char.value) + ((hash << 5) - hash)
+        }
+        
+        let finalHash = abs(hash) % (256 * 256 * 256);
+        let r = Double((finalHash & 0xFF0000) >> 16) / 255.0
+        let g = Double((finalHash & 0xFF00) >> 8) / 255.0
+        let b = Double((finalHash & 0xFF)) / 255.0
+        
+        return Color(red: r, green: g, blue: b)
     }
 }
