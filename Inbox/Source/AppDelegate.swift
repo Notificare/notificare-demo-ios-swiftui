f95e82ed429cb2f68cb680b4467374a4f2eecfa1
Helder Pinhal <helder@notifica.re>
2020-05-13 10:29:49 +0100

    Add missing initialisation

--- Inbox/Source/AppDelegate.swift
+++ Inbox/Source/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NotificarePushLibDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        NotificarePushLib.shared().initialize(withKey: nil, andSecret: nil)
         NotificarePushLib.shared().delegate = self
         NotificarePushLib.shared().launch()
         
